2b5118379e9e166ad85dc3778d837e5c7dd14732
paula dixon <paulapdixon@gmail.com>
2021-03-19 10:22:24 -0700

    added counter

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 
 import './App.css'
 import Name from './components/Name'
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
-function App() {
+export default function App() {
 
-
+  const [ count, setCount ] = useState(0)
 
   return (
-    <div className="App">
+    <div 
+      className="App"
+      style={{}}
+    >
+      {count}
+      <button onClick={() => setCount(currentCount => currentCount - 1)}>-</button>
+      <button onClick={() => setCount(currentCount => currentCount - 1)}>+</button>
       <Name/>
     </div>
-  );
+  )
 }
-
-export default App;
