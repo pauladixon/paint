01b1eb506d59c614befb2472866470df112f2840
paula dixon <paulapdixon@gmail.com>
2021-03-19 10:12:28 -0700

    rendering changeable painting name on page

---
+++ src/components/Name.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react'
+
+export default function Name() {
+
+    const [ name, setName ] = useState('')
+
+    return (
+        <label className='header-name'>
+            <input 
+                value = { name }
+                onChange = {e => setName(e.target.value)}
+                onClick = {e => e.target.setSelectionRange(0, e.target.value.length)}
+                placeHolder = 'untitled'
+            />
+        </label>
+    )
+}
