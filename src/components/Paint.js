9a316519beecd99eb02064f76d642162df259043
paula dixon <paulapdixon@gmail.com>
2021-03-19 13:02:21 -0700

    added window resize effect and canvas paint functionality

--- src/components/Paint.js
+++ src/components/Paint.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react'
 import Name from './Name'
 import ColorPicker from './ColorPicker'
 import randomColor from 'random-color'
+import Canvas from './Canvas'
+import WindowSize from './WindowSize'
 
 export default function Paint() {
 
@@ -23,17 +25,26 @@ export default function Paint() {
     useEffect(getColors, [])
 
     return (
-        <header style = {{ borderTop: `10px solid ${activeColor}`}}>
-            <div className='app'>
-                <Name />
-            </div>
-            <div style={{ marginTop: 10 }}>
-                <ColorPicker
-                    colors = {colors}
-                    activeColor = {activeColor}
-                    setActiveColor = {setActiveColor}
+        <div className='app'>
+            <header style = {{ borderTop: `10px solid ${activeColor}`}}>
+                <div className='app'>
+                    <Name />
+                </div>
+                <div style={{ marginTop: 10 }}>
+                    <ColorPicker
+                        colors = {colors}
+                        activeColor = {activeColor}
+                        setActiveColor = {setActiveColor}
+                    />
+                </div>
+            </header>
+            {activeColor && (
+                <Canvas
+                    color={activeColor}
+                    height={window.innerHeight}
                 />
-            </div>
-        </header>
+            )}
+            <WindowSize/>
+        </div>
     )
 }
